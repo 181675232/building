2100862ee0fcf2257ceab76b673058ed71f53212
181675232 <181675232@qq.com>
2016-08-22 18:25:40 +0800

    Rejected commit: Default

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -37,7 +37,7 @@ return array(
 	'DATA_CACHE_PREFIX' => 'building',
 		
 	/*Redis设置*/
-	'REDIS_HOST'            => '101.200.81.192', //主机
+	'REDIS_HOST'            => '127.0.0.1', //主机
 	'REDIS_PORT'            => '6379', //端口
 	'REDIS_CTYPE'           => 1, //连接类型 1:普通连接 2:长连接
 	'REDIS_TIMEOUT'         => 0, //连接超时时间(S) 0:永不超时
