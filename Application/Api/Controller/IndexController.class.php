29d10c59b85664b522e9dbc876991ba34409fdfb
181675232 <181675232@qq.com>
2016-09-10 10:22:13 +0800

    Rejected commit: Default

--- Application/Api/Controller/IndexController.class.php
+++ Application/Api/Controller/IndexController.class.php
@@ -1198,8 +1198,8 @@ class IndexController extends CommonController {
 
     //群组成员
     public function groupsuser(){
-        $where['proid'] = I('post.proid') ? I('post.proid') : json('404','缺少参数 proid');
-        $where['groups_id'] = I('post.groups_id') ? I('post.groups_id') : json('404','缺少参数 groups_id');
+        $where['t_groupsuser.proid'] = I('post.proid') ? I('post.proid') : json('404','缺少参数 proid');
+        $where['t_groupsuser.groups_id'] = I('post.groups_id') ? I('post.groups_id') : json('404','缺少参数 groups_id');
         $table = M('groupsuser');
         $data = $table->field('t_admin.id,t_groupsuser.level,t_admin.username,t_admin.simg,t_level.name')
             ->join('left join t_admin on t_admin.id = t_groupsuser.user_id')
